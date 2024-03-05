b074c31198d7f7274ab5c3844fa830ad2485d264
lucas <lucas@gmail.com>
2024-03-05 14:16:37 -0300

    calculadora01

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'CALCULADORA SIMPLES';
+  appVersion = '2.3.4';
+  num01 = 10;
+  num02 = 20;
+}
